20bea01205e5e3d440078d044ec59f3e71ada8ad
Kneatrum <martin.mwiti.n@gmail.com>
2025-11-16 12:36:33 +0300

    Set up a prompt for user to confirm irrigation schedule changes

--- src/glcd.h
+++ src/glcd.h
@@ -25,4 +25,5 @@ void turnOnDisplay();
 void showDetailsScreen(RTCTime_t* currentTime, irrigationTime_t* schedule, bool* valveState);
 void showIrrigationTimeSetting(irrigationTime_t* schedule, IrrigationTimeEditingField editingField);
 void showSystemTimeSetting(systemTime_t* systemTime, const char * const Months[], SystemTimeEditingField editingField);
+void promptUserSaveNewSchedule(irrigationTime_t schedule, StateMachine sm);
 #endif
